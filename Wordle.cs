ee084db259881c348aac05175dd149321734577a
Muhammad Ashhal <muhammadashhal22@gmail.com>
2026-02-11 01:07:12 +0500

    Validate Function Complete

--- Wordle.cs
+++ Wordle.cs
@@ -1,21 +1,47 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.Marshalling;
 class Wordle
 {
-    private string? word;
+    private string? words;
 
-    public void validate()
+    public bool validate(string input)
     {
+        string path = "words.txt"; // File name (same folder as program)
+
+        if (File.Exists(path))
+        {
+            string[] words = File.ReadAllLines(path);
+            foreach (string word in words)
+        {
+            if (word.Equals(input, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+        return false;
+        }
+        else {return false;}
         
     }
-
 }
 class Program
 {
     static void Main()
     {
-        bool run = true;
-        while (run) {
-            
+        Wordle w1 = new Wordle();
+        Console.WriteLine("Enter a word: ");
+        string? word = Console.ReadLine();
+
+        if (word != null && w1.validate(word))
+        {
+        // Program will continue
         }
-    }
+        else{Console.WriteLine("galat");}
+            // Selction of a random word
+            
+            // Validation() Word h ya nahi
+            // calculation() Conditions check
+            // 
+        }
 }
