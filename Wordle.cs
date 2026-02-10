304d72ffd609df01fe0bc62bbd35141c17ac5131
Muhammad Ashhal <muhammadashhal22@gmail.com>
2026-02-10 14:05:26 +0500

    Wordle Beginning

---
+++ Wordle.cs
@@ -0,0 +1,21 @@
+using System;
+class Wordle
+{
+    private string? word;
+
+    public void validate()
+    {
+        
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        bool run = true;
+        while (run) {
+            
+        }
+    }
+}
