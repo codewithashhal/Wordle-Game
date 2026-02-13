72e8a0be4b2b85eda8330c7d3520ec6dce370c5c
Muhammad Ashhal <muhammadashhal22@gmail.com>
2026-02-13 16:10:19 +0500

    Made input & Calculate function

--- Wordle.cs
+++ Wordle.cs
@@ -4,6 +4,18 @@ using System.Runtime.InteropServices.Marshalling;
 class Wordle
 {
     private string? words;
+    private string comp = "";
+
+    public string randomInput()
+    {
+        Random random = new Random();
+        int number = random.Next(0, 51);
+        string path = "words.txt";
+        string[] words = File.ReadAllLines(path);
+        comp += words[number].ToLower();
+        return comp;
+
+    }
 
     public bool validate(string input)
     {
@@ -13,35 +25,75 @@ class Wordle
         {
             string[] words = File.ReadAllLines(path);
             foreach (string word in words)
-        {
-            if (word.Equals(input, StringComparison.OrdinalIgnoreCase))
             {
-                return true;
+                if (word.Equals(input, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else { return false; }
+
+    }
+
+    public void calculate(string input)
+    {
+        
+        for (int i = 0; i < input.Length; i++)
+        {
+            bool found = false;
+            if (comp[i] == input[i])
+            {
+                Console.WriteLine("Green " + comp[i]);
+            }
+            else
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    
+                    if (input[i] != comp[j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yellow " + comp[j]);
+                        found = true;   
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("Grey");
+                }
             }
         }
-        return false;
-        }
-        else {return false;}
-        
+
     }
 }
 class Program
 {
     static void Main()
     {
+
         Wordle w1 = new Wordle();
         Console.WriteLine("Enter a word: ");
         string? word = Console.ReadLine();
+        Console.WriteLine(w1.randomInput());
 
         if (word != null && w1.validate(word))
         {
-        // Program will continue
+            // Program will continue
         }
-        else{Console.WriteLine("galat");}
-            // Selction of a random word
-            
-            // Validation() Word h ya nahi
-            // calculation() Conditions check
-            // 
-        }
+        else { Console.WriteLine("galat"); }
+        // Selction of a random word
+
+        // Validation() Word h ya nahi
+        // calculation() Conditions check
+        //
+
+        w1.calculate(word);
+    }
+
+
 }
